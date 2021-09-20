850e2a79682e99b93c46e29155e0d3dae2d2486e
anil verma <45934268+anilvermaspeaks@users.noreply.github.com>
2021-09-20 10:07:45 +0530

    Project init for party events

---
+++ pages/events/add.js
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout'
+
+
+export default function add() {
+    return (
+        <Layout title="Add New Event">
+            <h1>Add Event</h1>
+        </Layout>
+    )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout'
+
+
+export default function index() {
+    return (
+        <Layout>
+            <h1>Events</h1>
+        </Layout>
+    )
+}

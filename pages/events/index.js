e47541cd04771fab2361acb2c16a030e4d05dc53
anil verma <anilcs240@gmail.com>
2021-09-24 08:14:44 +0530

    added local api in api folder and fetched server data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,28 @@
-import Layout from '../../components/Layout'
+import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
+import Event from '@/components/Event'
 
-
-export default function index() {
+export default function HomePage({ events }) {
     return (
-        <Layout>
+        <Layout title="Events page">
             <h1>Events</h1>
+            {events.length === 0 && <h3>No Events to show!</h3>}
+
+            {events.length > 0 && events.map(evt => <Event key={evt.id} evt={evt} />)}
         </Layout>
     )
+}
+
+
+export async function getStaticProps() {
+
+    const res = await fetch(`${API_URL}/api/events`)
+    const events = await res.json()
+
+    return {
+        props: {
+            events: events.events
+        },
+        revalidate: 1
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
-import Layout from '../components/Layout'
+import Link from 'next/link'
+import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
+import Event from '@/components/Event'
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events to show!</h3>}
+
+      {events.length > 0 && events.map(evt => <Event key={evt.id} evt={evt} />)}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
     </Layout>
   )
+}
+
+
+export async function getStaticProps() {
+
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: {
+      events: events.events.slice(0, 3)
+    },
+    revalidate: 1
+  }
 }
